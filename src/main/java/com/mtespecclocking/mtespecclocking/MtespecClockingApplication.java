0bfe173c0dac796626427ea12dea63605f4bd677
Paloma Pereira <paloma.pereira@outlook.com>
2020-08-04 20:32:40 -0300

    initial commit

---
+++ src/main/java/com/mtespecclocking/mtespecclocking/MtespecClockingApplication.java
@@ -0,0 +1,13 @@
+package com.mtespecclocking.mtespecclocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtespecClockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtespecClockingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtespecclocking/mtespecclocking/MtespecClockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtespecclocking.mtespecclocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtespecClockingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
